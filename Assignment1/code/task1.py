8881e41e728a00af47c2cae8781fe773a12530a7
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-10-08 09:44:45 -0400

    AddedNewDataset

--- Assignment1/code/task1.py
+++ Assignment1/code/task1.py
@@ -38,133 +38,136 @@ vaersVaxCOVIDID2023 =vaersIDHashmapCovid("../dataset/2023Vaers/2023VAERSVAX.csv"
 vaersVaxNonDomestics =vaersIDHashmapCovid("../dataset/NonDomestics/NONDomesticVAERSVAX.csv")[0]
 print(len(vaersVaxCOVIDID2020)+len(vaersVaxCOVIDID2021)+len(vaersVaxCOVIDID2022)+len(vaersVaxCOVIDID2023)+len(vaersVaxNonDomestics))
 # ================================================================================================================================
-# def vaersSymptomsHashmapCovid(hasmap, filepath):
-#     # creating a hasmap
-#     vaersSymptomsHasmap = defaultdict(list)
-#     r = open(filepath, "r", encoding="ISO-8859-1")
-#     reader = csv.reader(r)
-#     headinglist = []
-#     i=0
-#     for x in reader:
-#         if i==0:
-#             headinglist = x[1:]
-#             i+=1
+def vaersSymptomsHashmapCovid(hasmap, filepath):
+    # creating a hasmap
+    vaersSymptomsHasmap = defaultdict(list)
+    r = open(filepath, "r", encoding="ISO-8859-1")
+    reader = csv.reader(r)
+    headinglist = []
+    i=0
+    for x in reader:
+        if i==0:
+            headinglist = x[1:]
+            i+=1
+            continue
         
-#         singlelinearray = x
+        singlelinearray = x
         
-#         if singlelinearray[0] in hasmap:
+        if int(singlelinearray[0]) in hasmap:
            
-#             vaersSymptomsHasmap[singlelinearray[0]].append(singlelinearray[1:])
-#     return [vaersSymptomsHasmap, headinglist]
+            vaersSymptomsHasmap[int(singlelinearray[0])].append(singlelinearray[1:])
+    return [vaersSymptomsHasmap, headinglist]
 
 
-# vaersSymptomCovid2020Both =vaersSymptomsHashmapCovid(vaersVaxCOVIDID2020,"../dataset/2020Vaers/2020VAERSSYMPTOMS.csv")
-# vaersSymptomCovid2020 = vaersSymptomCovid2020Both[0]
-# vaersSymptomHeading = vaersSymptomCovid2020Both[1]
-# vaersSymptomCovid2021 =vaersSymptomsHashmapCovid(vaersVaxCOVIDID2021,"../dataset/2021Vaers/2021VAERSSYMPTOMS.csv")[0]
-# vaersSymptomCovid2022 =vaersSymptomsHashmapCovid(vaersVaxCOVIDID2022,"../dataset/2022Vaers/2022VAERSSYMPTOMS.csv")[0]
-# vaersSymptomCovid2023 =vaersSymptomsHashmapCovid(vaersVaxCOVIDID2023,"../dataset/2023Vaers/2023VAERSSYMPTOMS.csv")[0]
-# vaersNonDomesticsSymptoms =vaersSymptomsHashmapCovid(vaersVaxNonDomestics,"../dataset/NonDomestics/NonDomesticVAERSSYMPTOMS.csv")[0]
+vaersSymptomCovid2020Both =vaersSymptomsHashmapCovid(vaersVaxCOVIDID2020,"../dataset/2020Vaers/2020VAERSSYMPTOMS.csv")
+vaersSymptomCovid2020 = vaersSymptomCovid2020Both[0]
+vaersSymptomHeading = vaersSymptomCovid2020Both[1]
+vaersSymptomCovid2021 =vaersSymptomsHashmapCovid(vaersVaxCOVIDID2021,"../dataset/2021Vaers/2021VAERSSYMPTOMS.csv")[0]
+vaersSymptomCovid2022 =vaersSymptomsHashmapCovid(vaersVaxCOVIDID2022,"../dataset/2022Vaers/2022VAERSSYMPTOMS.csv")[0]
+vaersSymptomCovid2023 =vaersSymptomsHashmapCovid(vaersVaxCOVIDID2023,"../dataset/2023Vaers/2023VAERSSYMPTOMS.csv")[0]
+vaersNonDomesticsSymptoms =vaersSymptomsHashmapCovid(vaersVaxNonDomestics,"../dataset/NonDomestics/NonDomesticVAERSSYMPTOMS.csv")[0]
 # # Assignment1/dataset/NonDomestics/NonDomesticVAERSSYMPTOMS.csv
-
+# print(len(vaersSymptomCovid2020)+len(vaersSymptomCovid2021)+len(vaersSymptomCovid2022)+len(vaersSymptomCovid2023)+len(vaersNonDomesticsSymptoms))
 # # # # ============================================================================================================================
-# def vaersDataHashmapCovid(hasmap, filepath):
-#     i = 0
-#     vaersDataHasmap = {}
-#     r = open(filepath, "r", encoding="ISO-8859-1")
-#     reader = csv.reader(r)
-#     headinglist = []
+def vaersDataHashmapCovid(hasmap, filepath):
+    i = 0
+    vaersDataHasmap = {}
+    r = open(filepath, "r", encoding="ISO-8859-1")
+    reader = csv.reader(r)
+    headinglist = []
     
-#     for x in reader:
+    for x in reader:
         
-#         if i==0:
-#             headinglist =x
-#             i+=1
-#         singllinearray =x
-#         if singllinearray[0] in hasmap:
-#             vaersDataHasmap[singllinearray[0]] = singllinearray
-#     return [vaersDataHasmap, headinglist]
+        if i==0:
+            headinglist =x
+            i+=1
+            continue
+        singllinearray =x
+        if int(singllinearray[0]) in hasmap:
+            vaersDataHasmap[int(singllinearray[0])] = singllinearray
+    return [vaersDataHasmap, headinglist]
 
 
-# vaersDataCovid2020Both =vaersDataHashmapCovid(vaersVaxCOVIDID2020,"../dataset/2020Vaers/2020VAERSDATA.csv")
-# vaersDataCovid2020 = vaersDataCovid2020Both[0]
-# vaersDataHeading = vaersDataCovid2020Both[1]
-# vaersDataCovid2021 =vaersDataHashmapCovid(vaersVaxCOVIDID2021,"../dataset/2021Vaers/2021VAERSDATA.csv")[0]
-# vaersDataCovid2022 =vaersDataHashmapCovid(vaersVaxCOVIDID2022,"../dataset/2022Vaers/2022VAERSDATA.csv")[0]
-# vaersDataCovid2023 =vaersDataHashmapCovid(vaersVaxCOVIDID2023,"../dataset/2023Vaers/2023VAERSDATA.csv")[0]
-# vaersNonDomesticsDataCovid =vaersDataHashmapCovid(vaersVaxNonDomestics,"../dataset/NonDomestics/NonDomesticVAERSDATA.csv")[0]
-
+vaersDataCovid2020Both =vaersDataHashmapCovid(vaersVaxCOVIDID2020,"../dataset/2020Vaers/2020VAERSDATA.csv")
+vaersDataCovid2020 = vaersDataCovid2020Both[0]
+vaersDataHeading = vaersDataCovid2020Both[1]
+vaersDataCovid2021 =vaersDataHashmapCovid(vaersVaxCOVIDID2021,"../dataset/2021Vaers/2021VAERSDATA.csv")[0]
+vaersDataCovid2022 =vaersDataHashmapCovid(vaersVaxCOVIDID2022,"../dataset/2022Vaers/2022VAERSDATA.csv")[0]
+vaersDataCovid2023 =vaersDataHashmapCovid(vaersVaxCOVIDID2023,"../dataset/2023Vaers/2023VAERSDATA.csv")[0]
+vaersNonDomesticsDataCovid =vaersDataHashmapCovid(vaersVaxNonDomestics,"../dataset/NonDomestics/NonDomesticVAERSDATA.csv")[0]
 
 # # # # ===========================================================================================================================================================================
 # # # merging 3 different hasmap into single hasmap
-# def singleYearCovidData(hasmapVaersData, hasmapVaersSymptoms, hasmapVaersCovid):
-#     vaerSingleYearcovid = {}
-#     for x in hasmapVaersData:
-#         vaerSingleYearcovid[x]=[hasmapVaersData[x],hasmapVaersSymptoms[x],hasmapVaersCovid[x]]
-#     return vaerSingleYearcovid
+def singleYearCovidData(hasmapVaersData, hasmapVaersSymptoms, hasmapVaersCovid):
+    vaerSingleYearcovid = {}
+    for x in hasmapVaersData:
+        vaerSingleYearcovid[x]=[hasmapVaersData[x],hasmapVaersSymptoms[x],hasmapVaersCovid[x]]
+    return vaerSingleYearcovid
 
 
-# covidAllthree2020 = singleYearCovidData(vaersDataCovid2020,vaersSymptomCovid2020,vaersVaxCOVIDID2020 ) 
-# covidAllthree2021 = singleYearCovidData(vaersDataCovid2021,vaersSymptomCovid2021,vaersVaxCOVIDID2021 ) 
-# covidAllthree2022 = singleYearCovidData(vaersDataCovid2022,vaersSymptomCovid2022,vaersVaxCOVIDID2022 ) 
-# covidAllthree2023 = singleYearCovidData(vaersDataCovid2023,vaersSymptomCovid2023,vaersVaxCOVIDID2023 ) 
-# covidAllthreeNonDomestics = singleYearCovidData(vaersNonDomesticsDataCovid,vaersNonDomesticsSymptoms,vaersVaxNonDomestics)
+covidAllthree2020 = singleYearCovidData(vaersDataCovid2020,vaersSymptomCovid2020,vaersVaxCOVIDID2020 ) 
+covidAllthree2021 = singleYearCovidData(vaersDataCovid2021,vaersSymptomCovid2021,vaersVaxCOVIDID2021 ) 
+covidAllthree2022 = singleYearCovidData(vaersDataCovid2022,vaersSymptomCovid2022,vaersVaxCOVIDID2022 ) 
+covidAllthree2023 = singleYearCovidData(vaersDataCovid2023,vaersSymptomCovid2023,vaersVaxCOVIDID2023 ) 
+covidAllthreeNonDomestics = singleYearCovidData(vaersNonDomesticsDataCovid,vaersNonDomesticsSymptoms,vaersVaxNonDomestics)
+
 
 # # # merging 4 hasmap into single hasmap
-# covidAllthree2020.update(covidAllthree2021)
-# covidAllthree2020.update(covidAllthree2022)
-# covidAllthree2020.update(covidAllthree2023)
-# covidAllthree2020.update(covidAllthreeNonDomestics)
-# allthreeYearsCovid = covidAllthree2020
+covidAllthree2020.update(covidAllthree2021)
+covidAllthree2020.update(covidAllthree2022)
+covidAllthree2020.update(covidAllthree2023)
+covidAllthree2020.update(covidAllthreeNonDomestics)
+allFileCovid = covidAllthree2020
+
 
 # # # # =============================================================================================================================
 # # # # vaersID 1106890 = does not have any symptoms
 
 # # # # using allthreeYearsCovid to create a new csv file
 
-# outputfilename = "VAERS_COVID_DataAugust2023.csv"
-# r = open(outputfilename,"w")
-# writer = csv.writer(r)
-# heading = vaersDataHeading+vaersVaxHeading+vaersSymptomHeading
+outputfilename = "VAERS_COVID_DataAugust2023.csv"
+r = open(outputfilename,"w")
+writer = csv.writer(r)
+heading = vaersDataHeading+vaersVaxHeading+vaersSymptomHeading
 
-# writer.writerow(heading)
-# for x in allthreeYearsCovid:
+writer.writerow(heading)
+for x in allFileCovid:
    
-#      # mergingintosingleline
-#      currentVaersId = x
-#      currentVaersData = allthreeYearsCovid[currentVaersId]
-#      vaersData = currentVaersData[0]
-#      vaersSympt = currentVaersData[1]
-#      vaersType = currentVaersData[2]
-#      newvaersSympt=[]
-#      if len(vaersSympt)==0:
-#         #  vaersID 1106890 = does not have any symptoms
-#          newvaersSympt=["-","-","-","-","-","-","-","-","-","-","-"]
-#      else:
+     # mergingintosingleline
+     currentVaersId = x
+     currentVaersData = allFileCovid[currentVaersId]
+     vaersData = currentVaersData[0]
+     vaersSympt = currentVaersData[1]
+     vaersType = currentVaersData[2]
+     newvaersSympt=[]
+     if len(vaersSympt)==0:
+        #  vaersID 1106890 = does not have any symptoms
+         newvaersSympt=["-","-","-","-","-","-","-","-","-","-","-"]
+     else:
        
-#          for y in range(len(vaersSympt[0])):
-#              combinetxt=""
-#              for x in range(len(vaersSympt)):
-#                  combinetxt += vaersSympt[x][y]+" "
-#              newvaersSympt.append(combinetxt)
-#      vaersSympt=newvaersSympt
+         for y in range(len(vaersSympt[0])):
+             combinetxt=""
+             for x in range(len(vaersSympt)):
+                 combinetxt += vaersSympt[x][y]+" "
+             newvaersSympt.append(combinetxt)
+     vaersSympt=newvaersSympt
 
 
-#      newvaersType=[]
+     newvaersType=[]
    
-#      for c in range(len(vaersType[0])):
-#          combinetxt=""
-#          for d in range(len(vaersType)):
-#              combinetxt+=vaersType[d][c]+" "
-#          newvaersType.append(combinetxt)
-#      vaersType = newvaersType
-#      row = vaersData+vaersType+vaersSympt
-#      writer.writerow(row)
+     for c in range(len(vaersType[0])):
+         combinetxt=""
+         for d in range(len(vaersType)):
+             combinetxt+=vaersType[d][c]+" "
+         newvaersType.append(combinetxt)
+     vaersType = newvaersType
+     row = vaersData+vaersType+vaersSympt
+     writer.writerow(row)
 
 # # # # # //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 # # # # # TASK2
 # # # # # VAERS_ID, AGE_YRS, SEX, VAX_NAME, RPT_Date, SYMPTOM, DIED, DATEDIED,
-# headinglist2  = ["VAERS_ID","AGE_YRS","SEX","VAX_NAME","RPT_Date","SYMPTOM","DIED","DATEDIED","SYMPTOMS TEXT"]
+headinglist2  = ["VAERS_ID","AGE_YRS","SEX","VAX_NAME","RPT_Date","SYMPTOM","DIED","DATEDIED","SYMPTOMS TEXT"]
 
 # # # # vaersid = [0]
 # # # # ageyears= [3]
@@ -176,42 +179,42 @@ print(len(vaersVaxCOVIDID2020)+len(vaersVaxCOVIDID2021)+len(vaersVaxCOVIDID2022)
 # # # # datedied= [10]
 # # # # symptomtext = [8]
 
-# outputfilename2 = "SYMPTOMDATA.csv"
-# r2 = open(outputfilename2,"w")
-# writer = csv.writer(r2)
-# writer.writerow(headinglist2)
+outputfilename2 = "SYMPTOMDATA.csv"
+r2 = open(outputfilename2,"w")
+writer = csv.writer(r2)
+writer.writerow(headinglist2)
 
-# def task2datasetYearly(vaersData, vaersSymptoms, vaersVax):
-#     for x in vaersSymptoms:
-#         for y in vaersSymptoms[x]:
-#             i=0
-#             for z in y:
-#                 if i>len(y)-1:
-#                     break
-#                 if len(y[i])==0:
-#                     break
+def task2datasetYearly(vaersData, vaersSymptoms, vaersVax):
+    for x in vaersSymptoms:
+        for y in vaersSymptoms[x]:
+            i=0
+            for z in y:
+                if i>len(y)-1:
+                    break
+                if len(y[i])==0:
+                    break
 
-#                 if len(y[i])>1:
-#                     vaersId = vaersData[x][0]
-#                     ageyrs = vaersData[x][3]
-#                     sex = vaersData[x][6]
-#                     vaxname = vaersVax[x][0][6]
-#                     rptdate = vaersData[x][7]
-#                     symptom = y[i]
-#                     died = vaersData[x][9]
-#                     datedied = vaersData[x][10]
-#                     symptomstext= vaersData[x][8]
-#                     row=[vaersId, ageyrs, sex, vaxname, rptdate, symptom, died, datedied,symptomstext]
-#                     writer.writerow(row)
+                if len(y[i])>1:
+                    vaersId = vaersData[x][0]
+                    ageyrs = vaersData[x][3]
+                    sex = vaersData[x][6]
+                    vaxname = vaersVax[x][0][6]
+                    rptdate = vaersData[x][7]
+                    symptom = y[i]
+                    died = vaersData[x][9]
+                    datedied = vaersData[x][10]
+                    symptomstext= vaersData[x][8]
+                    row=[vaersId, ageyrs, sex, vaxname, rptdate, symptom, died, datedied,symptomstext]
+                    writer.writerow(row)
 
-#                 i+=2
+                i+=2
 
       
-# a = task2datasetYearly(vaersDataCovid2020, vaersSymptomCovid2020, vaersVaxCOVIDID2020)
-# b = task2datasetYearly(vaersDataCovid2021, vaersSymptomCovid2021, vaersVaxCOVIDID2021)
-# c = task2datasetYearly(vaersDataCovid2022, vaersSymptomCovid2022, vaersVaxCOVIDID2022)
-# d = task2datasetYearly(vaersDataCovid2023, vaersSymptomCovid2023, vaersVaxCOVIDID2023)
-# e = task2datasetYearly(vaersNonDomesticsDataCovid,vaersNonDomesticsSymptoms,vaersVaxNonDomestics)
+a = task2datasetYearly(vaersDataCovid2020, vaersSymptomCovid2020, vaersVaxCOVIDID2020)
+b = task2datasetYearly(vaersDataCovid2021, vaersSymptomCovid2021, vaersVaxCOVIDID2021)
+c = task2datasetYearly(vaersDataCovid2022, vaersSymptomCovid2022, vaersVaxCOVIDID2022)
+d = task2datasetYearly(vaersDataCovid2023, vaersSymptomCovid2023, vaersVaxCOVIDID2023)
+e = task2datasetYearly(vaersNonDomesticsDataCovid,vaersNonDomesticsSymptoms,vaersVaxNonDomestics)
 
 
 
--- Assignment1/code/task2.py
+++ Assignment1/code/task2.py
@@ -1,5 +1,6 @@
 import csv
 import random
+import time
 
 
 def read_csv(filename):
@@ -13,10 +14,7 @@ def read_csv(filename):
 a = read_csv("./SYMPTOMDATA.csv")
 # ========================================================================================================================
 def insertion_sort(data, field):
-     a = 1
      for i in range(1, len(data)):
-        print(a)
-        
       
         pivot = int(data[i][field])
         j = i - 1
@@ -24,48 +22,60 @@ def insertion_sort(data, field):
              data[j + 1] = data[j]
              j -= 1
         data[j + 1][field] = pivot
-        a+=1
+     
 # =========================================================================================================================
     
 def bubble_sort(data, field):
     n = len(data)
-    a=0
     for i in range(n - 1):
-        print(a)
         swapped=False
         for j in range(n - i - 1):
             if int(data[j][field]) > int(data[j + 1][field]):
                 data[j], data[j + 1] = data[j + 1], data[j]
                 swapped=True
         if not swapped:
+            print("DONE WITH BUBBLE SORT")
             return 
-        a+=1
     print("DONT WITH BUBBLE SORT")
 # # =====================================================================================
-def quick_sort(data, field, low, high):
+def partition(data, field, low, high):
     if low < high:
-        print(high-low)
         pivot_index = partition(data, field, low, high)
-        quick_sort(data, field, low, pivot_index)
+        quick_sort(data, field, low, pivot_index-1)
         quick_sort(data, field, pivot_index + 1, high)
 
-# # Function to partition the data for quick sort
-def partition(data, field, low, high):
-    # index = (high-low)
-    pivot = int(data[low][field])
-    left = low
-    right = high
 
-    while True:
-        while int(data[left][field]) < pivot:
-            left += 1
-        while int(data[right][field]) > pivot:
-            right -= 1
-        if left >= right:
-            return right
-        data[left], data[right] = data[right], data[left]
-        left += 1
-        right -= 1
+# # Function to partition the data for quick sort
+def quick_sort(data, field, low, high):
+    pivotindex = random.randint(low,high)
+    pivot = int(data[pivotindex][field])
+    left = low+1
+    end = high
+    while left<=end:
+        while left<=end and int(data[left][field]) < pivot:
+            l+=1
+        while data[end]>pivot and int(data[end][field]) > pivot:
+            end-=1
+        if left<end:
+            temp = data[l]
+            data[l] = data[end]
+            data[end] = temp
+    temp = pivot
+    data[pivotindex] = data[end]
+    data[end] = temp
+    return end
+
+    # while True:
+    #     while int(data[left][field]) < pivot:
+    #         left += 1
+    #     while int(data[right][field]) > pivot:
+    #         right -= 1
+    #     if left >= right:
+    #         return right
+    #     data[left], data[right] = data[right], data[left]
+    #     left += 1
+    #     right -= 1
+    
 # =================================================================================================================
 def write_sorted_csv(data, filename):
     r= open(filename, 'w', newline='')
@@ -75,11 +85,14 @@ def write_sorted_csv(data, filename):
     for row in data:
         writer.writerow(row)
 # ========================================================================================================================
+
 a = read_csv("./SYMPTOMDATA.csv")
-print(len(a))
-b = quick_sort(a, "VAERS_ID", 0, len(a)-1)
+starttime = time.time()
+# b = partition(a, "VAERS_ID", 0, len(a)-1)
+# print("THE TOTAL TIME FOR QUICK SORT TO RUN FOR " ,len(a)," MANY DATA IS "+ str(time.time()-starttime))
 b = insertion_sort(a, "VAERS_ID")
-# b = bubble_sort(a[0:500], "VAERS_ID")
+# print("THE TOTAL TIME FOR INSERTION SORT TO RUN FOR " ,len(a)," MANY DATA IS "+ str(int(time.time()-starttime)))
+# b = bubble_sort(a, "VAERS_ID")
 
 # # write_sorted_csv(b, "SORTEDSYMPTOM.CSV")
 # # {'VAERS_ID': '0902418', 'AGE_YRS': '56.0', 'SEX': 'F', 'VAX_NAME': 'COVID19 (COVID19 (PFIZER-BIONTECH))', 'RPT_Date': '', 'SYMPTOM': 'Hypoaesthesia', 'DIED': '', 'DATEDIED': '', 'SYMPTOMS TEXT': 'Patient experienced mild numbness traveling from injection site up and down arm that subsided over 20 minutes.'}
